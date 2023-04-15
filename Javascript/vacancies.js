c0f7218b8917860e6b14b66f33f3b5e8c69dfc4b
Kym-e <83183893+Kym-e@users.noreply.github.com>
2023-04-16 00:34:51 +0100

    Started on vacancies api

--- Javascript/vacancies.js
+++ Javascript/vacancies.js
@@ -3,7 +3,6 @@ function vacanciesApiCall(evt) {
     evt.preventDefault();
 
     let jobSearch = document.getElementById("vacancies-search-bar").value;
-    // let baseUrl = "https://api.lmiforall.org.uk/api/v1/vacancies/search";
     let baseUrl = "https://api.lmiforall.org.uk/api/v1/vacancies/search?keywords=";
 
     let baseUrlJobDescriptions = "https://api.lmiforall.org.uk/api/v1/soc/search";
@@ -44,22 +43,21 @@ function vacanciesApiCall(evt) {
 
 function displayJobDetails(data) {
     let dataLength = data.length;
-    let text = "<section>";
-    dataTitle = data.title;
+
+    let text;
+    text = "<h1>Results</h1>";
+    text += "<hr>"
 
     for (let i = 0; i < 5; i++){
         // displayJobDetails(data);
-        text += "<h3>";
-        text += document.getElementById("title").innerHTML = data[i].title;
-        text += "</h3>";
+        text += "<h2>";
+        text += data[i].title;
+        text += "</h2>";
         console.log(data[i])
-
     }
-    text += "</section>"
-
 
     // console.log(data)
     document.getElementById("vacancies-results").style.display = "block";
-    document.getElementById("title").innerHTML = text;
+    document.getElementById("vacancies-results").innerHTML = text;
 }
 
